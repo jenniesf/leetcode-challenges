b2e8a3e499f7102d5a979ed3a8e43dafcff32547
Jennie <99220339+jenniesf@users.noreply.github.com>
2022-06-04 21:31:57 -0700

    Add files via upload

---
+++ leetcode-6.04.22.js
@@ -0,0 +1,20 @@
+// Two Sum
+function twoSum (nums, target) {
+
+      for ( let i = 0 ; i < nums.length ; i++) {
+
+            for (let j = i+1; j < nums.length ; j++) {
+
+                  let sum = nums[i] + nums[j]
+
+                  if (sum == target) {
+                      return [ i , j ]
+                  }
+
+            }
+      }
+}
+
+console.log(  twoSum(  [ 3,2,4 ] ,  6  ))
+
+
